ec89eeadd3046fd190098690a9e17f2e8cac960d
maerdaevahi <39660242+maerdaevahi@users.noreply.github.com>
2019-05-03 00:22:20 +0800

    Update Class_1.java
    1222

--- src/com/sword/model/Class_1.java
+++ src/com/sword/model/Class_1.java
@@ -5,4 +5,10 @@ public class Class_1 {
 		System.out.println("aaaaaaaaaaaaaaaaaaaaaaa");
 		System.out.println("bbbbbbbbbbbbbbbbbbbbbbb");
 	}
+	public void method_1() {
+		System.out.println("111111111111111111111111111");
+	}
+	public void method_1() {
+		System.out.println("22222222222222222222222222222");
+	}
 }
